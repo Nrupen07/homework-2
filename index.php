ce9cf96e02ea9ef2ba77fd8e5ea8e0c9d6cae7ee
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-19 12:10:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <!doctype html>
 <html lang = "en">
 <body>
- <nav class="navbar bg-light">
+ <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      PHP Homework
-    </a>
+    <a class="navbar-brand" href="#">PHP Reference</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
   <h1> Welcome to Homework 2!! </h1>
