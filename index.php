479c921c35e7f2c58eb99b6b91c9e2636029a76e
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-19 13:03:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
  
-  <h1 style="background-color:DodgerRed;"> Welcome to Homework 2!! (PHP Reference with use of POST and GET functions) </h1>
+  <h1 sstyle="color:Red;> Welcome to Homework 2!! (PHP Reference with use of POST and GET functions) </h1>
    <br> </br>
-   <h2 style="background-color:DodgerGray;"> GET </h3>
+   <h2 sstyle="color:Gray;"> GET </h3>
    
   <form action="welcome_get.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
      <br> </br>
-  <h3 style="background-color:DodgerGray;"> POST </h2>
+  <h3 style="color:Gray;"> POST </h2>
   
 <form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
