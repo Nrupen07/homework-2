d340782198318f790580e9d89d53066927c16c43
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-19 11:36:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
+  <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 </form>
 </html>
 </body>
