d91ca6f4c8474a6305333537cb6b355d56735575
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-19 11:42:04 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,6 +1,10 @@
 <html>
   <body>
-    
+    <form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
     Welcome <?php echo $_POST["name"]; ?><br>
     your email address is: <?php echo $_POST["email"]; ?>
     
