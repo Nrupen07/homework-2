be601b33182ac8639b78374fab99903f263583e6
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-19 12:56:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
  
-  <h1> Welcome to Homework 2!! </h1>
+  <h1> Welcome to Homework 2!! (PHP Reference with use of POST and GET functions) </h1>
    <br> </br>
   <h2> POST </h2>
   
