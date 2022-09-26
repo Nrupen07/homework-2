db20173d9bd1af1d303e436c048642b12de927b7
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-26 14:06:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">  
    <br> </br>
    <h3 style="color:Gray;"> GET </h3>
+   </form>
    
   <form action="handleget.php" method="get">
 Name: <input type="text" name="name"><br>
@@ -27,7 +28,7 @@ E-mail: <input type="text" name="email"><br>
 <input type="submit">
   
   
-</form>
+
 </form>
 </html>
 </body>
