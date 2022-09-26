42bf794ba2d35b2e3253fad457618fbfcb858b2a
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-26 14:14:34 -0500

    Update header.php

--- header.php
+++ header.php
@@ -44,3 +44,15 @@
     </div>
   </div>
 </nav>
+    
+<!doctype html>
+<html lang = "en">
+<body>
+   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+ 
+  <h1 style="color:Red;"> Welcome to Homework 2!! (PHP Reference with use of POST and GET functions) </h1>
