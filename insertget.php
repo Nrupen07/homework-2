0bf0d980d27ca77098470c98eac20562fb51cc0c
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-26 14:33:57 -0500

    Update insertget.php

--- insertget.php
+++ insertget.php
@@ -1,5 +1,6 @@
 
-   
+     <h1 style="color:Gray;"> GET </h1>
+<br> </br>
   <form action="handleget.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
