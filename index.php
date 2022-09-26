5bc0aacb9620a46ed8ba3c59b561f9420b20326c
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-26 14:29:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<?php insert "header.php"; 
-insert "Postpage.html";
-insert "Getpage.html";
+<?php include "header.php"; 
+include "Postpage.html";
+include "Getpage.html";
 ?>
 
 
 
-
-   <h3 style="color:Gray;"> GET </h3>
-   </form>
-   
-  <form action="handleget.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-  
-  </form>
 
 </body>
 </html>
