a043ec08f54faa217116361c6d8210c5981033a2
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-09-26 14:17:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php require_once("header.php"); ?>
 
-<!doctype html>
-<html lang = "en">
-<body>
-   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  </head>
- 
-  <h1 style="color:Red;"> Welcome to Homework 2!! (PHP Reference with use of POST and GET functions) </h1>
-   <br> </br>
+
   <h2 style="color:Gray;"> POST </h2>
   
 <form action="handlepost.php" method="post">
@@ -19,6 +8,7 @@ Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">  
    <br> </br>
+
    <h3 style="color:Gray;"> GET </h3>
    </form>
    
@@ -27,8 +17,7 @@ Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
   
-  
+  </form>
 
-</form>
-</html>
 </body>
+</html>
